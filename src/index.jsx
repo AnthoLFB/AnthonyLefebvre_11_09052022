dac6ef3c3487d64a6ca2584c16128799e4ddae0d
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-09 13:50:44 +0200

    Ajout des routes redirigeant vers les différentes pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,35 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//Pages
 import Home from './pages/Home';
+import Accommodation from './pages/Accommodation';
+import AboutUs from './pages/AboutUs';
+import Error from './pages/Error';
+
+//Router
+import { BrowserRouter as Router, Routes as RoutesList, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <RoutesList>
+        
+        {/*Accueil*/}
+        <Route path="/" element={<Home/>}></Route>
+        
+        {/*Logement*/}
+        <Route path="/logement/:id" element={<Accommodation/>}></Route>
+
+        {/*A propos*/}
+        <Route path="/a-propos" element={<AboutUs/>}></Route>
+
+        {/*Page d'erreur*/}
+        <Route path="*" element={<Error/>}></Route>
+        
+      </RoutesList>
+    </Router>
   </React.StrictMode>
 );
