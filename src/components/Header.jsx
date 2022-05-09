216afeea6adbbe326460825bfbaa4800e7901841
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-09 15:47:18 +0200

    Création du composant Header. Il est composé d'une image (logo), d'une barre de navigation avec des liens.

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+//Liens
+import { NavLink } from "react-router-dom";
+
+
+//Images
+import logo from '../assets/images/logo.jpg';
+
+//CSS
+import '../styles/Header.css';
+
+function Header()
+{
+    return (
+        <header>
+            <img className="header__image" src={logo} alt="Kasa"/>
+            <nav className="header__navbar">
+                <ul className="header__navbar__list">
+                    <li className="header__navbar__list__item"><NavLink className="header__navbar__list__item__link" to="/">Accueil</NavLink></li>
+                    <li className="header__navbar__list__item"><NavLink className="header__navbar__list__item__link" to="/a-propos">A propos</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
