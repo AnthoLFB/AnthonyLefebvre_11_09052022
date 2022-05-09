f0f6d973e8fb504a5fc5119d3adb073be28dff5e
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-09 18:43:34 +0200

    Ajout du composant dans la page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 
 //Composants
 import Header from '../../components/Header';
+import Banner from '../../components/Banner';
+
+//Images
+import coastalLandscape from '../../assets/images/coastalLandscape.jpg';
 
 function Home() {
   return (
     <React.Fragment>
       <Header/>
+      <Banner image={coastalLandscape} itHaveText={true} text="Chez vous, partout et ailleurs"/>
     </React.Fragment>
   );
 }
