ae6bc1c1524b2c8782f3cc8ccd6885330340d589
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-10 16:40:51 +0200

    Création du composant contenant le contenu d'une annonce

---
+++ src/components/AccommodationsListItem.jsx
@@ -0,0 +1,19 @@
+//Liens
+import {Link} from 'react-router-dom'
+
+//CSS
+import '../styles/AccommodationsList.css' 
+
+function AccommodationsListItem({id, title, cover})
+{
+    return (
+        <Link className="accommodationsList__card__link" to={`logement/${id}`}>
+            <img className="accommodationsList__card__link__image" src={cover} alt={title}/>
+            <div className="accommodationsList__card__link__container">
+                <h1 className="accommodationsList__card__link__container__title">{title}</h1>
+            </div>
+        </Link>
+    );
+}
+
+export default AccommodationsListItem
