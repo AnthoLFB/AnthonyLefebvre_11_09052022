ca3154e7766562e3af27b389b60dd557d2fe8831
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-12 15:04:32 +0200

    Création du composant Collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,36 @@
+//React
+import { useState } from 'react'
+
+//Images
+import up from "../assets/images/up.svg"
+import down from "../assets/images/down.svg"
+
+//CSS
+import '../styles/Collapse.css'
+
+function Collapse({title, text})
+{
+    const [isOpen, setIsOpen] = useState(false);
+
+    if(isOpen)
+    {
+        return (
+            <section className="collapse">
+                <button className="collapse__button" type="button" onClick={() => setIsOpen(false)}><h2 className="collapse__button__title">{title}</h2><img src={up} alt="Fermer"/></button>
+                <div className="collapse__container">
+                    <p className="collapse__container__content">{text}</p>
+                </div>
+            </section>
+        )
+    }
+    else
+    {
+        return (
+            <section className="collapse">
+                <button className="collapse__button" type="button" onClick={() => setIsOpen(true)}><h2 className="collapse__button__title">{title}</h2><img src={down} alt="Ouvrir"/></button>
+            </section>
+        )
+    }
+}
+
+export default Collapse
