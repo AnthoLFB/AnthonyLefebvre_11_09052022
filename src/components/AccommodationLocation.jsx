03ef11d9078230fc7bbc47528e00e8e55f3fb5d3
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-15 18:08:55 +0200

    Ajout du composant permettant de voir les informations concernant le logement

---
+++ src/components/AccommodationLocation.jsx
@@ -0,0 +1,20 @@
+//Composants
+import Tag from "./Tag";
+
+//CSS
+import "../styles/AccommodationLocation.css"
+
+function AccommodationLocation({title, location, tags})
+{
+    return (
+        <section className="accommodation-information__location">
+            <h1 className="accommodation-information__location__title">{title}</h1>
+            <h2 className="accommodation-information__location__address">{location}</h2>
+            <ul className="accommodation-information__location__tags-list">
+                {tags.map((tag, index) => <li key={`${tag}-${index}`}className="accommodation-information__location__tags-list__item">{<Tag name={tag}/>}</li>)}
+            </ul>	
+        </section>
+    )
+}
+
+export default AccommodationLocation
