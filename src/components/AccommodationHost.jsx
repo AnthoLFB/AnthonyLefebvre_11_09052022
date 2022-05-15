78c733c536f7e0dc0d2091f5b1a0507fff9ce6f6
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-15 18:07:53 +0200

    Ajout du composant permettant de voir les informations de l'hote

---
+++ src/components/AccommodationHost.jsx
@@ -0,0 +1,20 @@
+//Composants
+import Star from "./Star";
+
+//CSS
+import "../styles/AccommodationHost.css";
+
+function AccommodationHost({host, rating})
+{
+    return (
+        <section className="accommodation-information__host">
+            <div className="accommodation-information__host__personal-data-container">
+                <p className="accommodation-information__host__personal-data-container__name">{host.name}</p>
+                <img className="accommodation-information__host__personal-data-container__image" src={host.picture} alt={`Profil de ${host.name}`}/>
+            </div>
+            <p className="accommodation-information__host__rating-container">{<Star rating={rating} maxRating={5}/>}</p>
+        </section>
+    )
+}
+
+export default AccommodationHost
