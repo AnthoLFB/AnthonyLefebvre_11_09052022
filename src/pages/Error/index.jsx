dbeb21fb7e953622ffef3af0268c53647eb06952
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-15 19:38:25 +0200

    Import du composant error

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,18 @@
-function Error() {
+//React
+import React from 'react';
+
+//Composant
+import Header from '../../components/Header';
+import Error from "../../components/Error";
+
+function ErrorPage() {
     return (
-      <h1>Page d'erreur</h1>
+      <React.Fragment>
+        <Header/>
+        <Error errorCode={"404"} errorMessage={"Oups! La page que vous demandez n'existe pas."}/>
+      </React.Fragment>
     );
   }
   
-  export default Error;
+  export default ErrorPage;
   
