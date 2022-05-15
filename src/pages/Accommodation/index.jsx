df3548ad537c5f54cef7c9fb4d18493b8048ba58
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-15 14:55:36 +0200

    Import des composants

--- src/pages/Accommodation/index.jsx
+++ src/pages/Accommodation/index.jsx
@@ -1,8 +1,19 @@
-function Accommodation() {
-    return (
-      <h1>Page du logement</h1>
-    );
-  }
+//React
+import React from 'react';
+
+//Composants
+import Header from '../../components/Header';
+import AccommodationInformation from '../../components/AccommodationInformation';
+
+function Accommodation() 
+{
+  return (
+    <React.Fragment>
+      <Header/>
+      <AccommodationInformation/>
+    </React.Fragment>
+  );
+}
   
   export default Accommodation;
   
