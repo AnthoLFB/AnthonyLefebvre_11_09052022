b3d9affa9a3a09d3cb88241cf65c50c9bfcf75b8
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2022-05-15 19:38:03 +0200

    Création du composant Error

---
+++ src/components/Error.jsx
@@ -0,0 +1,19 @@
+//ReactRouter
+import {Link} from "react-router-dom";
+
+//CSS
+import "../styles/Error.css";
+import "../styles/responsive/Error.css";
+
+function Error ({errorCode, errorMessage})
+{
+    return  (
+        <section className="error">
+            <h1 className="error__code">{errorCode}</h1>
+            <h2 className="error__message">{errorMessage}</h2>
+            <Link className="error__link" to="/">Retourner sur la page d’accueil</Link>
+        </section>
+    )
+}
+
+export default Error
